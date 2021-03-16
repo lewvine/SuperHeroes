8b9a5223e5fb5650205c1c3b518e4887d1bd23b5
Lewis Vine <lew.vine@gmail.com>
2021-03-16 13:31:10 -0400

    Added SuperHero model with properties

---
+++ SuperHeroes/Models/SuperHero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroes.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string CatchPhrase { get; set; }
+
+    }
+}
