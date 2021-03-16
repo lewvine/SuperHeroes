0c223051750cc56e8c19b02b064c8df92056f009
Lewis Vine <lew.vine@gmail.com>
2021-03-16 13:36:32 -0400

    Added DdSet<SuperHero>

--- SuperHeroes/Data/ApplicationDbContext.cs
+++ SuperHeroes/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SuperHeroes.Models;
 
 namespace SuperHeroes.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<SuperHero> SuperHeroes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
